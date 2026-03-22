f5dac837f1e048a4b0d1fbea087d61d70575caa8
Juho Oksa <juho.oksa@gmail.com>
2026-03-22 18:08:52 +0200

    Add computer screen scene and basic parsing logic

---
+++ computer_screen.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+@onready var lineEdit = $LineEdit
+@onready var printOut = $Label
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("enter"):
+		parse_text(lineEdit.text)
+		lineEdit.text = ""
+
+func parse_text(command: String):
+	command.to_lower()
+	match command:
+		"help":
+			write_to_print_out("Use 'open' command to open the door.")
+		"open":
+			write_to_print_out("Opening the door.")
+		"":
+			write_to_print_out(" ")
+		_:
+			write_to_print_out("Command: '" + command + "' not found.")
+
+func write_to_print_out(text: String):
+	var oldPrint = printOut.text
+	print(oldPrint)
+	var newPrint = oldPrint + "\n" + text
+	printOut.text = newPrint
